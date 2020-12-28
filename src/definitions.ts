ebd256242ab51b8fc4f1c10769f1353b1f280876
no-dap <somniholic@naver.com>
2020-12-28 13:54:27 +0900

    Fix spec

--- src/definitions.ts
+++ src/definitions.ts
@@ -13,20 +13,24 @@ declare module '@capacitor/core' {
 }
 
 export interface NotificationExtensionPlugin extends PushNotificationsPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
   /**
    * @deprecated Use getToken() instead.
    */
   addListener(eventName: 'registration', listenerFunc: (token: PushNotificationToken) => void): PluginListenerHandle;
+
   addListener(eventName: 'registrationError', listenerFunc: (error: any) => void): PluginListenerHandle;
+
   addListener(eventName: 'pushNotificationReceived', listenerFunc: (notification: PushNotification) => void): PluginListenerHandle;
+
   addListener(eventName: 'pushNotificationActionPerformed', listenerFunc: (notification: PushNotificationActionPerformed) => void): PluginListenerHandle;
+
   /**
    * Get FCM token from client with promise.
    * Use this method to get token rather than waiting 'registration' event listener
    * to control the procedure of initializing application.
    */
   getToken(): Promise<{ value: string }>
+
   /**
    * Save the range of time to SQLite to filtering push notification when it is received.
    * Push notification won't shown when current time is out of range.
@@ -34,11 +38,24 @@ export interface NotificationExtensionPlugin extends PushNotificationsPlugin {
    * @param options
    */
   addTimeFilter(options: { startFrom: string, endAt: string }): Promise<void>
+
+  /**
+   * Remove time filter which added with addTimeFilter method.
+   */
+  removeTimeFilter(): Promise<void>
+
   /**
    * Save the key - boolean field to SQLite to filtering push notification when it is received.
-   * Push notification won't shown when any of the value from matched keys exists.
+   * Push notification won't shown when any of the value from matched keys exists with 'false' value.
    * Can detailed filter with saved value.
    * @param options
    */
-  addFilter(options: { key: string, value: string }): Promise<void>
+  addFilters(options: { filters: string[] }): Promise<void>
+
+  /**
+   * Find all rows that have key matched with filters and make value to 'true'
+   * to not ignore push notification.
+   * @param options
+   */
+  removeFilters(options: { filters: string[] }): Promise<void>
 }
