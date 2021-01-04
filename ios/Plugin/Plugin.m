2b39b650ee8d1e07131e11eb1803fc86f74d92c7
mjLee <audwn0326@gmail.com>
2021-01-04 17:40:58 +0900

    done? - ios notification extension

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,9 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(NotificationExtension, "NotificationExtension",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getToken, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(addTimeFilter, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(removeTimeFilter, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(addFilters, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(removeFilters, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import FirebaseMessaging
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -8,6 +9,7 @@ import Capacitor
 @objc(NotificationExtension)
 public class NotificationExtension: CAPPlugin {
     let sqlHandler = SQLiteHandler()
+
     public override func load() {
         NotificationCenter.default.addObserver(
             self,
@@ -16,55 +18,104 @@ public class NotificationExtension: CAPPlugin {
             object: nil
         )
     }
-
+    
     @objc func onNotification(_ notification: Notification) {
-        sqlHandler.openDB()
         sqlHandler.createFilterTable()
-        addNewNotification(notification: notification)
+        handleNotification(notification: notification)
     }
     
-    @objc func addNewNotification(notification: Notification) {
-        print(notification)
-        UNUserNotificationCenter.current().getDeliveredNotifications { notifications in
-            let identifier = String(Int.random(in: 0..<5))
-            var badge = notifications.count
-            // get delivered notification duplicated width identifier
-            let duplitedNotificationIdentifier = notifications.filter { (noti: UNNotification) -> Bool in
-                let notificationRequest = noti.value(forKey: "request") as! UNNotificationRequest
-                let notificationIdentifier = notificationRequest.value(forKey: "identifier")
-                return notificationIdentifier as! String == identifier
+    @objc func handleNotification(notification: Notification) -> Void {
+        // extract push data. if there is no data, return.
+        guard let notificationData: [String: Any] = notification.userInfo?["aps"] as? [String: Any] else {
+            return
+        }
+
+        // notify notification data to listner
+        notifyToListeners(identifier: "pushNotificationReceived", data: notificationData)
+        
+        if checkMessageCondition(remoteMessageData: notificationData) {
+            UNUserNotificationCenter.current().getDeliveredNotifications { deliveredNotifications in
+                // if there is no identifier, finish this process
+                guard let identifier: String = self.getIdentifier(notificationData) else {
+                    return
+                }
+                let content: UNMutableNotificationContent = self.getPushNotificationContent(notificationData, deliveredNotifications, identifier)
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                
+                self.showPushNotification(request: request)
             }
-            // if there is no duplicated delivered notifications, plus badge count
-            if duplitedNotificationIdentifier.count == 0 {
-                badge += 1
+        }
+    }
+    
+    @objc func getIdentifier(_ data: [String: Any]) -> String? {
+        if let identifier: String = data["filter"] as? String {
+            return identifier
+        } else {
+            return nil
+        }
+    }
+    
+    // get push notification content
+    @objc func getPushNotificationContent(_ data: [String: Any], _ deliveredData: [UNNotification], _ identifier: String) -> UNMutableNotificationContent {
+        let title: String = (data["title"] ?? "") as! String // title
+        let body: String = (data["body"] ?? "") as! String // body
+        // default badge value == 1
+        var badge: Int = (data["badge"] as? Int) ?? 1
+        // if there is duplicated notification, badge count is same as before
+        if let _: UNNotification = getDuplicatedNotification(data: deliveredData, identifier: identifier) {
+            badge = getDeliveredBadgeCount(data: deliveredData)
+        } else {
+            badge += getDeliveredBadgeCount(data: deliveredData)
+        }
+        let content = UNMutableNotificationContent()
+        content.title = title
+//        content.subtitle = subtitle
+        content.body = body
+        content.badge = badge as NSNumber
+        content.sound = UNNotificationSound.default
+        return content
+    }
+    
+    // get duplicated notification data from notifications which have been sent and not read(== delivered notifications).
+    @objc func getDuplicatedNotification(data: [UNNotification], identifier: String) -> UNNotification? {
+        let duplicatedNotifications: [UNNotification?] = data.filter { (noti: UNNotification) -> Bool in
+            let notificationRequest = noti.value(forKey: "request") as! UNNotificationRequest
+            if let notificationIdentifier: String = notificationRequest.value(forKey: "identifier") as? String {
+                return notificationIdentifier == identifier
+            } else {
+                return false
             }
-            let notificationData = notification.userInfo?["aps"] as! [String: Any]
-            let title = (notificationData["hello"] ?? "") as! String
-            let subtitle = (notificationData["foo"] ?? "") as! String
-            let body = (notificationData["bar"] ?? "") as! String
-//            let url = (notificationData["url"] ?? "") as! String
-            let content = UNMutableNotificationContent()
-            print("sync test")
-            content.title = title
-            content.subtitle = subtitle
-            content.body = body
-            content.badge = badge as NSNumber
-            content.sound = UNNotificationSound.default
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-            let isActive = self.isApplicationActive()
-            if (isActive) {
-                content.badge = 0
-                content.sound = nil
+        }
+        if duplicatedNotifications.count > 0 {
+            if let result: UNNotification = duplicatedNotifications[0] {
+                return result
             }
-            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        }
+        return nil
+    }
+    
+    // get count of delivered notifications
+    @objc func getDeliveredBadgeCount(data: [UNNotification]) -> Int {
+        return data.count
+    }
+    
+    // show push notification to user
+    @objc func showPushNotification(request: UNNotificationRequest) -> Void {
+        // send push only inactive state
+        let isActive = self.isApplicationActive()
+        if !isActive {
             UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         }
     }
     
-    @objc func afterPushCompleted() {
-        print("completed")
+    @objc func notifyToListeners(identifier: String, data: [String: Any]) -> Void {
+        notifyListeners(identifier, data: data)
     }
     
+    /**
+     * get is application active ( return true when application is in foreground )
+     */
     @objc func isApplicationActive() -> Bool {
         DispatchQueue.main.sync {
             switch UIApplication.shared.applicationState {
@@ -77,58 +128,185 @@ public class NotificationExtension: CAPPlugin {
     }
     
     @objc func getToken(_ call: CAPPluginCall) {
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            call.error("Failed to get instance FirebaseID", error)
+          } else if let token = token {
+            call.success([
+                "token": token
+            ]);
+          }
+        }
+    }
+
+    @objc func addTimeFilter(_ call: CAPPluginCall) -> Void {
+        // add timeFilter only when PluginCall has "startFrom" and "endAt"
+        if let startFrom = call.getString("startFrom"), let endAt = call.getString("endAt") {
+            handleResult(result: sqlHandler.insertTimeFilter(startFrom: startFrom, endAt: endAt), call)
+        }
+    }
+
+    @objc func removeTimeFilter(_ call: CAPPluginCall) -> Void {
+        handleResult(result: sqlHandler.removeTimeFilter(), call)
+    }
+
+    @objc func addFilters(_ call: CAPPluginCall) -> Void {
+        guard let filters = call.getArray("filters", String.self) else {
+            call.reject("filters undefined")
+            return;
+        }
+
+        for filter in filters {
+            let result = isSucceedResult(result: sqlHandler.insertFilter(key: filter), call)
+            if !result {
+                return;
+            }
+        }
+        call.success()
+    }
+
+    @objc func removeFilters(_ call: CAPPluginCall) -> Void {
+        guard let filters = call.getArray("filters", String.self) else {
+            call.reject("filters undefined")
+            return;
+        }
         
-    }
-
-    @objc func addTimeFilter(_ call: CAPPluginCall) {
-        sqlHandler.openDB()
-        let startFrom = call.getString("startFrom")
-        let endAt = call.getString("endAt")
-//        let result = sqLiteHandler.insertTimeFilter(startFrom, endAt)
-//        if (result.getValue("success") as Boolean) {
-//            call.success()
-//        } else {
-//            call.reject(result.getValue("reason") as String)
-//        }
-    }
-
-    @objc func removeTimeFilter(_ call: CAPPluginCall) {
-        sqlHandler.openDB()
-//        let result = sqLiteHandler.removeTimeFilter()
-//        if (result.getValue("success") as Boolean) {
-//            call.success()
-//        } else {
-//            call.reject("Got an unexpected error while remove the time filter.")
-//        }
-    }
-
-    @objc func addFilters(_ call: CAPPluginCall) {
-        sqlHandler.openDB()
-        guard let filters = call.getArray("filters", String.self) else {
-            call.reject("filters undefined")
-            return;
+        for filter in filters {
+            let result = isSucceedResult(result:  sqlHandler.removeFilter(key: filter), call)
+            if !result {
+                return;
+            }
         }
-//        for (filter in filters) {
-//            let result = sqLiteHandler.insertFilter(filter)
-//            if (!(result.getValue("success") as Boolean)) {
-//                call.reject(result.getValue("reason") as String)
-//            }
-//        }
-//        call.success()
+        call.success()
     }
 
-    @objc func removeFilters(_ call: CAPPluginCall) {
-        sqlHandler.openDB()
-        guard let filters = call.getArray("filters", String.self) else {
-            call.reject("filters undefined")
-            return;
+    @objc func handleResult(result: [String: Any], _ call: CAPPluginCall) {
+        if result["success"] as! Bool {
+            call.success()
+        } else {
+            if let reason: String = result["reason"] as? String {
+                call.reject(reason)
+            } else {
+                call.reject("Something when wrong")
+            }
+            
         }
-//        for (filter in filters) {
-//            let result = sqLiteHandler.removeFilter(filter)
-//            if (!(result.getValue("success") as Boolean)) {
-//                call.reject(result.getValue("reason") as String)
-//            }
-//        }
-//        call.success()
+    }
+    
+    @objc func isSucceedResult(result: [String: Any], _ call: CAPPluginCall) -> Bool {
+        let boolResult = result["success"] as! Bool
+        if !boolResult {
+            if let reason: String = result["reason"] as? String {
+                call.reject(reason)
+            } else {
+                call.reject("Something when wrong")
+            }
+        }
+        return boolResult
+    }
+    
+    /**
+     * return current time only hours and minutes ( ex. HH:mm )
+     */
+    @objc func getCurrentTimeString() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.string(from: date)
+    }
+
+    /**
+     * return input time >= comparison time
+     */
+    @objc func compareTimeString(_ input: String, _ comparison: String) -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.date(from: input)! >= dateFormatter.date(from: comparison)!
+    }
+
+    @objc func isValidTime() -> Bool {
+        sqlHandler.createFilterTable()
+        let currentTime = getCurrentTimeString()
+        let timeFilters = sqlHandler.getTimeFilter()
+
+        func getFilteredData(filterString: String) -> Array<[String: Any]> {
+            return timeFilters.filter { (filter: [String: Any]) -> Bool in
+                if let key: String = filter["key"] as? String {
+                    return key == filterString
+                }
+                return false
+            }
+        }
+        let startTimeFilter: Array<[String: Any]> = getFilteredData(filterString: "filter_start_from")
+        let endTimeFilter: Array<[String: Any]> = getFilteredData(filterString: "filter_end_at")
+        let isTimeFilterOnObject: Array<[String: Any]> = getFilteredData(filterString: "is_time_filter_on")
+
+        // if there is no time filter or filterOn == false -> return true (can receive push-noti anytime)
+        if isTimeFilterOnObject.count >= 0 {
+            if let timeFilterOn: String = isTimeFilterOnObject[0]["value"] as? String {
+                if timeFilterOn == "false" {
+                    return true
+                }
+            }
+        }
+        if startTimeFilter.count != 0 && endTimeFilter.count != 0 {
+            guard let startFrom: String = startTimeFilter[0]["value"] as? String,
+                  let endAt: String = endTimeFilter[0]["value"] as? String else {
+                return false
+            }
+            return compareTimeString(currentTime, startFrom) && compareTimeString(endAt, currentTime)
+        } else {
+            return true
+        }
+    }
+
+    @objc func isValidCondition(_ remoteMessageData: [String: Any]) -> Bool {
+        guard let filterString: String = remoteMessageData["filter"] as? String else {
+            // return true if there is no filter
+            return true
+        }
+        
+        let filterList: Array<String> = processFilterString(filterString: filterString)
+        sqlHandler.createFilterTable()
+        let savedFilters: Array<[String: Any]> = sqlHandler.getFilters()
+        let matchedFilters = savedFilters.filter { (filter: [String: Any]) -> Bool in
+            if let key = filter["key"] as? String {
+                return filterList.contains(key)
+            } else {
+                return false
+            }
+            
+        }
+        for matchedFilter in matchedFilters {
+            if let value = matchedFilter["value"] as? String {
+                return value == "true"
+            }
+        }
+        return true
+    }
+
+    @objc func checkMessageCondition(remoteMessageData: [String: Any]) -> Bool {
+        return shouldMessageShown(remoteMessageData) && isValidTime() && isValidCondition(remoteMessageData)
+    }
+
+    @objc func processFilterString(filterString: String) -> Array<String> {
+        var mutableFilterString: String = filterString
+        if (mutableFilterString.hasSuffix(",")) {
+            let endIndex: String.Index = mutableFilterString.index(mutableFilterString.startIndex, offsetBy: mutableFilterString.count - 1)
+            mutableFilterString = String(mutableFilterString[...endIndex])
+        }
+        return mutableFilterString.split(separator: ",").map({ (value: Substring) -> String in
+            return String(value.trimmingCharacters(in: .whitespacesAndNewlines))
+        })
+    }
+    
+    @objc func shouldMessageShown(_ remoteMessageData: [String: Any]) -> Bool {
+        if let messageShown: String = remoteMessageData["is_shown"] as? String {
+            return messageShown == "true"
+        } else {
+            return true
+        }
     }
 }
